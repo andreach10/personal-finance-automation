51d09ed9901245b799f1189b60c28df4cd489efd
Andrea Chamorro <andreacaro159@gmail.com>
2026-04-24 16:43:56 -0500

    Update create_tables.sql

--- 2.DDL/create_tables.sql
+++ 2.DDL/create_tables.sql
@@ -1,20 +1,24 @@
+-- Transaction category
 CREATE TABLE categories (
   cat_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   cat_name VARCHAR NOT NULL
 )
 
+-- Transaction subcategory
 CREATE TABLE subcategories (
   subcat_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   subcat_name VARCHAR NOT NULL,
   cat_id INT NOT NULL REFERENCES categories(cat_id)
 )
 
+-- Bank account
 CREATE TABLE accounts (
   acc_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   acc_name VARCHAR NOT NULL,
   acc_type VARCHAR NOT NULL
 )
 
+-- Budget
 CREATE TABLE budget (
   bgt_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   bgt_month DATE NOT NULL,
@@ -26,6 +30,7 @@ CREATE TABLE budget (
   CONSTRAINT qty_check CHECK ((qty_amount IS NOT NULL AND bgt_qty IS NOT NULL) OR (qty_amount IS NULL AND bgt_qty IS NULL))
 )
 
+-- Transactions
 CREATE TABLE transactions (
   txn_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   date TIMESTAMPTZ NOT NULL,
