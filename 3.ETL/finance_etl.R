6c260e1181933028cf49b1d2b8d0a076263ca473
Andrea Chamorro <andreacaro159@gmail.com>
2026-04-30 12:11:03 -0500

    Create finance_etl.R

---
+++ 3.ETL/finance_etl.R
@@ -0,0 +1,47 @@
+install.packages('googlesheets4')
+
+library(googlesheets4)
+library(janitor)
+library(tidyverse)
+
+# Importo los datos con el API de Tidyverse
+Bancolombia <- read_sheet('1LyuDdPbMk5irN5LexWJUFyz_s1iTCnqCv2UUbfzbHPI', sheet = 'Bancolombia')
+Lulo <- read_sheet('1LyuDdPbMk5irN5LexWJUFyz_s1iTCnqCv2UUbfzbHPI', sheet = 'Lulo')
+
+# Verifico que tengo las mismas columnas
+compare_df_cols(Bancolombia, Lulo)
+
+# Uno las bases
+transactions <- bind_rows(Bancolombia, Lulo)
+
+compare_df_cols(transactions)
+lapply(transactions, function(x) 
+  {sort(unique(x))})
+
+transactions$Subcategoria <- gsub("medicamento", "Medicamento", transactions$Subcategoria)
+
+# Creo mis variables .env
+usethis::edit_r_environ()
+
+host <- Sys.getenv("SUPABASE_HOST")
+port <- as.integer(Sys.getenv("SUPABASE_PORT"))
+database <- Sys.getenv("SUPABASE_DB")
+user <- Sys.getenv("SUPABASE_USER")
+password <- Sys.getenv("SUPABASE_PW")
+
+#Conecto al db
+library(DBI)
+library(RPostgres)
+
+con <- dbConnect(
+  RPostgres::Postgres(),
+  dbname = database,
+  host = host,
+  port = port,
+  user = user,
+  password = password
+)
+
+# Verifico la conexión y cierro
+dbListTables(con)
+dbDisconnect(con)
