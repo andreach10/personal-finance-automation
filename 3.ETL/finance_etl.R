91c60046ffa7cf25417dbe604b4e2846c8c8665e
Andrea Chamorro <andreacaro159@gmail.com>
2026-05-06 10:42:25 -0500

    Update finance_etl.R

--- 3.ETL/finance_etl.R
+++ 3.ETL/finance_etl.R
@@ -1,5 +1,3 @@
-install.packages('googlesheets4')
-
 library(googlesheets4)
 library(janitor)
 library(tidyverse)
@@ -18,7 +16,46 @@ compare_df_cols(transactions)
 lapply(transactions, function(x) 
   {sort(unique(x))})
 
+transactions <- transactions %>% 
+  mutate(producto = case_when(
+    Producto == 'T.Credito Bancolombia' ~ 'Bancolombia',
+    .default = 'Lulo Bank'
+  ),
+  cuenta = case_when(
+    Producto %in% c('T.Credito Bancolombia', 'Lulo Credito', 'T.Credito Lulo') ~ 'Credito',
+    .default = 'Debito'
+  ))
+
 transactions$Subcategoria <- gsub("medicamento", "Medicamento", transactions$Subcategoria)
+
+# Creo mis db
+
+paste(unique(Bancolombia$Categoria), collapse = "', '")
+categories <- data.frame(
+  cat_name = c("Comida", "Tienda TQ", "Compras", "Tequi", "Casa", "Servicios", 
+               "Transporte", "Carro", "Entretenimiento", "Viaje", "Suscripciones", 
+               "Educación", "Hobbies", "Eventos", "Belleza", "Salud", 
+               "Inversiones", "Ingreso", "Tarjeta de credito")
+)
+
+subcategories <- transactions %>% 
+  left_join(cat, join_by(Categoria == cat_name)) %>% 
+  select(Subcategoria, cat_id) %>% 
+  rename(subcat_name = Subcategoria) %>% 
+  distinct(subcat_name, cat_id)
+
+paste(unique(transactions$Producto), collapse = "', '")
+account <- transactions %>% 
+  distinct(producto, cuenta) %>% 
+  rename(acc_name = producto,
+         acc_type = cuenta)
+
+months <- seq(as.Date("2026-01-17"), by = "month", length.out = 5)
+categories <- unlist(cat$cat_id)
+month_combinations <- expand.grid(months, categories) %>% 
+  rename(month = Var1,
+         cat_id = Var2)
+class(month_combinations$month)
 
 # Creo mis variables .env
 usethis::edit_r_environ()
@@ -42,6 +79,14 @@ con <- dbConnect(
   password = password
 )
 
-# Verifico la conexión y cierro
+dbWriteTable(con, 'categories', categories, append = TRUE)
+dbWriteTable(con, 'subcategories', subcategories, append = TRUE)
+dbWriteTable(con, 'accounts', account, append = TRUE)
+
+cat <- dbReadTable(con, "categories")
+subcat <- dbReadTable(con, "subcategories")
+acc <- dbReadTable(con, "accounts")
+
+# Verifico la conexión
 dbListTables(con)
 dbDisconnect(con)
